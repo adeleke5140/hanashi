73dd5a399ac9a1cf31bacbc15a3aee78a2d991ea
Kenny <adelekekehinde06@gmail.com>
2025-07-13 21:37:24 +0100

    chore: remove unused props

--- packages/chrome/src/popup/components/header.tsx
+++ packages/chrome/src/popup/components/header.tsx
@@ -1,40 +1,9 @@
-export const Header = ({
-  showSettings,
-  setShowSettings,
-}: {
-  showSettings: boolean;
-  setShowSettings: (show: boolean) => void;
-}) => {
+export const Header = () => {
   return (
     <div className="flex p-3 pt-0 px-0 rounded-xl items-center justify-between mb-3">
       <h1 className="text-5xl  mx-auto  text-[var(--primary)] font-semibold select-none">
         Hanashi
       </h1>
-
-      {/* <button
-        type="button"
-        className="text-[0.8rem] rounded-lg focus:outline-none px-1"
-      >
-        <span className="size-5 inline-grid place-items-center">
-          <svg
-            aria-label="API Key Saved"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="size-3"
-          >
-            <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-            <circle cx="12" cy="12" r="3" />
-          </svg>
-        </span>
-      </button> */}
     </div>
   );
 };
