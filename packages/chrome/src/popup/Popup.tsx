8776ad3a7d66e49d591532436da3976e031542bd
Kenny <adelekekehinde06@gmail.com>
2025-08-09 20:06:12 +0100

    fix: formatting errors

--- packages/chrome/src/popup/Popup.tsx
+++ packages/chrome/src/popup/Popup.tsx
@@ -6,43 +6,43 @@ import { Header } from "./components/header";
 import { JapaneseInputForm } from "./components/japanese-input-form";
 
 const Popup = () => {
-  const [audioDataUrl, setAudioDataUrl] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [audioElement, setAudioElement] = useState<HTMLAudioElement | null>(
-    null,
-  );
-  const [showSettings, setShowSettings] = useState(false);
+	const [audioDataUrl, setAudioDataUrl] = useState<string | null>(null);
+	const [error, setError] = useState<string | null>(null);
+	const [audioElement, setAudioElement] = useState<HTMLAudioElement | null>(
+		null,
+	);
+	const [showSettings, setShowSettings] = useState(false);
 
-  useEffect(() => {
-    return () => {
-      if (audioElement) {
-        audioElement.pause();
-      }
-    };
-  }, [audioElement]);
+	useEffect(() => {
+		return () => {
+			if (audioElement) {
+				audioElement.pause();
+			}
+		};
+	}, [audioElement]);
 
-  return (
-    <main className="p-4 text-white bg-gray-100/50 border w-[343.08px]">
-      <Header  />
+	return (
+		<main className="p-4 text-white bg-gray-100/50 border w-[343.08px]">
+			<Header />
 
-      <JapaneseInputForm
-        setError={setError}
-        setShowSettings={setShowSettings}
-        setAudioDataUrl={setAudioDataUrl}
-        audioElement={audioElement}
-      />
+			<JapaneseInputForm
+				setError={setError}
+				setShowSettings={setShowSettings}
+				setAudioDataUrl={setAudioDataUrl}
+				audioElement={audioElement}
+			/>
 
-      {error && <ErrorUI error={error || "エラーが発生しました"} />}
+			{error && <ErrorUI error={error || "エラーが発生しました"} />}
 
-      {audioDataUrl && (
-        <AudioPlayer
-          src={audioDataUrl}
-          autoPlay={true}
-          onAudioElement={setAudioElement}
-        />
-      )}
-    </main>
-  );
+			{audioDataUrl && (
+				<AudioPlayer
+					src={audioDataUrl}
+					autoPlay={true}
+					onAudioElement={setAudioElement}
+				/>
+			)}
+		</main>
+	);
 };
 
 export default Popup;
