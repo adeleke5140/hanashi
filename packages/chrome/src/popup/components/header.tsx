bd98ff70a8acc5574b9492122e463614b76b7784
Kenny <adelekekehinde06@gmail.com>
2025-08-09 19:38:50 +0100

    refactor: enhance Header component by adding an emoji to the title

--- packages/chrome/src/popup/components/header.tsx
+++ packages/chrome/src/popup/components/header.tsx
@@ -1,8 +1,8 @@
 export const Header = () => {
   return (
     <div className="flex p-3 pt-0 px-0 rounded-xl items-center justify-between mb-3">
-      <h1 className="text-5xl  mx-auto  text-[var(--primary)] font-semibold select-none">
-        Hanashi
+      <h1 className="text-5xl mx-auto  text-[var(--primary)] font-semibold select-none">
+        Hanashi 🌸
       </h1>
     </div>
   );
