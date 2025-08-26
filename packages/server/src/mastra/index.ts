0ea017db2bba6a4beef836146574ea9df2397f64
Kenny <adelekekehinde06@gmail.com>
2025-08-26 23:13:26 +0100

    fix: lint error

--- packages/server/src/mastra/index.ts
+++ packages/server/src/mastra/index.ts
@@ -1,18 +1,16 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
-import { weatherWorkflow } from "./workflows/weather-workflow";
 import { transliterationAgent } from "./agents/transliteration-master";
 
 export const mastra = new Mastra({
-	workflows: { weatherWorkflow },
-	agents: { transliterationAgent },
-	storage: new LibSQLStore({
-		// stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-		url: ":memory:",
-	}),
-	logger: new PinoLogger({
-		name: "Mastra",
-		level: "info",
-	}),
+  agents: { transliterationAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: "Mastra",
+    level: "info",
+  }),
 });
