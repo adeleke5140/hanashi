379dd28570b897ab4457b63109096a021cc2c5d5
Kenny <adelekekehinde06@gmail.com>
2025-08-27 09:11:47 +0100

    feat: add non-existing workflow

--- packages/server/src/mastra/index.ts
+++ packages/server/src/mastra/index.ts
@@ -2,8 +2,10 @@ import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
 import { transliterationAgent } from "./agents/transliteration-master";
+import { weatherWorkflow } from "./workflows/weather-workflow";
 
 export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
   agents: { transliterationAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
