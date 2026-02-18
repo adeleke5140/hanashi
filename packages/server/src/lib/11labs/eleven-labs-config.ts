73fd45c936c0cadbf79e2983729bee1886c2db76
Kenny <adelekekehinde06@gmail.com>
2026-02-19 00:17:41 +0100

    feat: add ai utils

--- packages/server/src/lib/11labs/eleven-labs-config.ts
+++ packages/server/src/lib/11labs/eleven-labs-config.ts
@@ -1,24 +1,24 @@
 export const voiceConfig = {
-	male: {
-		asahi_id: "GKDaBI8TKSBJVhsCLD6n",
-	},
-	female: {
-		morioki_id: "8EkOjt4xTPGMclNlh1pk",
-		sakura_id: "RBnMinrYKeccY3vaUxlZ",
-		saanu_id: "50YSQEDPA2vlOxhCseP4",
-	},
+  male: {
+    asahi_id: "GKDaBI8TKSBJVhsCLD6n",
+  },
+  female: {
+    morioki_id: "8EkOjt4xTPGMclNlh1pk",
+    sakura_id: "RBnMinrYKeccY3vaUxlZ",
+    saanu_id: "50YSQEDPA2vlOxhCseP4",
+  },
 };
 
 export const defaultConfig = {
-	male: {
-		stability: 0.5,
-		similarity_boost: 0.14,
-		use_speaker_boost: true,
-	},
-	female: {
-		speed: 0.9,
-		stability: 0.5,
-		similarity_boost: 0.75,
-		use_speaker_boost: true,
-	},
+  male: {
+    stability: 0.5,
+    similarity_boost: 0.14,
+    use_speaker_boost: true,
+  },
+  female: {
+    speed: 0.9,
+    stability: 0.5,
+    similarity_boost: 0.75,
+    use_speaker_boost: true,
+  },
 };
--- packages/server/src/lib/ai/generate-text.ts
+++ packages/server/src/lib/ai/generate-text.ts
@@ -3,12 +3,12 @@ import { generateText } from "ai";
 import { systemPrompt } from "./prompt";
 
 export async function generateTransliteration(text: string, apiKey: string) {
-	const openai = createOpenAI({
-		apiKey,
-	});
-	return await generateText({
-		model: openai("gpt-4.1-mini-2025-04-14"),
-		system: systemPrompt,
-		prompt: text,
-	});
+  const openai = createOpenAI({
+    apiKey,
+  });
+  return await generateText({
+    model: openai("gpt-4.1-mini-2025-04-14"),
+    system: systemPrompt,
+    prompt: text,
+  });
 }
